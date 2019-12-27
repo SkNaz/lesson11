3145e91677b9a4b72226fc36fffcc06363986d19
SkNaz <whlzor@gmail.com>
2019-12-27 11:09:54 +0200

    l-13_v2

--- lesson13/index.js
+++ lesson13/index.js
@@ -1,14 +1,9 @@
 var area = document.querySelector(".txt");
+
 var tpp = document.querySelectorAll(".put");
+
 setInterval(function(){
-	var x = document.getElementById("lah").textContent;
-console.log(x);
-	
-}, 1000)
-
-
- /*for(i = 0; i < tpp.length; i++) {
-	 	area.textContent = get.tpp[i].textContent;
-	 }*/
-
-	 /*area.textContent = document.getElementById("lah").textContent;*/
+	for(i = 0; i < tpp.length; i++) {
+		area.textContent = tpp[i].value;
+	}	
+}, 5000)
