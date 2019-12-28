fd555b48e794303e881eb72e65c52e06d909b22a
SkNaz <whlzor@gmail.com>
2019-12-28 16:22:33 +0200

    l-13_v4

--- lesson13/index.js
+++ lesson13/index.js
@@ -3,10 +3,24 @@ var area = document.querySelector(".txt");
 var tpp = document.querySelectorAll(".put");
 
 setInterval(function(){	
+	for(i = 0, mass = []; i < tpp.length; i++) {
+		if(tpp[i].value) {
+			mass[i] = tpp[i].value;		
+		}
+		area.textContent = mass;
+	}	
+}, 1000)
+
+/*второй вариант без массива
+
+setInterval(function(){	
 	for(i = 0; i < tpp.length; i++) {
 		if(tpp[i].value) {
-			lal = tpp[i].value.split(",");
+			area.textContent += tpp[i].value + ",";		
 		}
-		area.textContent += lal;
 	}	
-}, 10000)
+}, 1000)
+
+Думаю, он хуже - ставит запятую и после текста  последнего ипнута + продолжает вписывать одно и тоже каждый интервал*/
+
+
