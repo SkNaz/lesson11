792e6c697dd2463ed6f0109d0c76955c90e02530
SkNaz <whlzor@gmail.com>
2020-01-09 13:10:11 +0200

    final

--- lesson14/induk.js
+++ lesson14/induk.js
@@ -1,28 +1,28 @@
 window.onload = function() {
 	var rak = document.querySelector(".box");
-
+	console.dir(rak);
+	console.dir(document);
 	rak.onclick = function() {	
 		this.classList.toggle("grach");
 	}
 
-	/*document.onclick = function(event) {
+	document.onclick = function(event) { 
 		console.log(event);
 		var go = event.clientX;
 		var no = event.clientY;
 		rak.style.top = no + "px";
 		rak.style.left = go + "px";
-	}*/
-
-	rak.onmousedown = function() {
-		document.addEventListener("mousemove", mousemove);
 	}
-	var mousemove = function() {
+
+	rak.onmousedown = function(event) {
+		var x = event.offsetX
+		var y = event.offsetY
 		document.onmousemove = function(event) {
-			rak.style.top = event.clientY + "px";
-			rak.style.left = event.clientX + "px";
+			rak.style.top = (event.clientY - y) + "px";
+			rak.style.left = (event.clientX - x) + "px";
 		}	
 	}
  	rak.onmouseup = function() {
-		document.removeEventListener("mousemove", mousemove);
-	}
+ 		document.onmousemove = null;
+ 	}
 }
