64dae7335d8cd5880c5d9c2b60cf35b3a533b043
SkNaz <whlzor@gmail.com>
2020-01-15 15:38:37 +0200

    working!

--- lesson17/index.js
+++ lesson17/index.js
@@ -1,6 +1,22 @@
 window.onload = function() {
 	var income = document.querySelectorAll(".fields_one");
-	var tt = document.querySelector("#test");
+	var what = document.querySelector(".wrap");
+	var haha = document.querySelectorAll(".lol");
+	var yo = document.querySelectorAll(".sub");
+	for (var i = 0; i < yo.length; i++) {
+		yo[i].onclick = (function(i) {
+			return function() {
+				var counter;
+				if(haha[i + 1]) {
+					counter = i + 1;
+				} else {
+					counter = 0;
+				}
+					haha[i].id = "";
+					haha[counter].id = "center";
+			}
+		})(i);
+	}
 
 	getItem();
 
@@ -18,9 +34,12 @@ window.onload = function() {
 		localStorage.setItem("block", json);
 	}
 
-	tt.onclick = function() {
-		setvalue();
-	}
+	what.addEventListener("click", function (event) {
+		var target = event.target;
+		if(target && target.dataset.quest) {
+			setvalue();
+		}
+	});
 
 	function getItem() {
 		var timoMass = JSON.parse(localStorage.getItem("block"));
