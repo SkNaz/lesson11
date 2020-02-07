f0e86c89ad6ef8e725b0abb3d8b441b2acf46158
SkNaz <whlzor@gmail.com>
2020-02-07 20:30:09 +0200

    v0.3

--- 21/21/index.js
+++ 21/21/index.js
@@ -1,16 +1,29 @@
 var zapr = new XMLHttpRequest();
 var login = document.querySelector(".text_one");
 var pass = document.querySelector(".text_two");
+var xxx = new XMLHttpRequest();
+var html = document.querySelector("body");
+
+
 
 zapr.onreadystatechange = function() {
 	if(zapr.readyState == 4) {
 		let data = zapr.responseText;
 		let parsedData = JSON.parse(data);
-		console.log(data);
+		console.log(parsedData);	
+	}
+}
+
+xxx.onreadystatechange = function() {
+	if(xxx.readyState == 4) {
+		let data = xxx.responseText;
+		let parsedData = JSON.parse(data);
+		console.log(parsedData);
 	}
 }
 
 zapr.open("post", "http://localhost:3000/user", true);
+xxx.open("post", "http://localhost:3000/goods", true);
 
 
 document.querySelector(".btn").onclick = function() {
@@ -20,4 +33,9 @@ document.querySelector(".btn").onclick = function() {
 	}
 
 	zapr.send(JSON.stringify(cred));
+	xxx.send("23");
+
+	setTimeout(function ping() {
+		html.innerHTML += "<div>" + xxx.responseText + "</div>"
+	}, 1000);
 }
