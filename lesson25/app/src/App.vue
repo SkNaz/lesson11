f9eeed3df9945cd145b8cbdea96a878752bc8b9d
SkNaz <whlzor@gmail.com>
2020-02-26 21:04:22 +0200

    v0.1

---
+++ lesson25/app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <button @click="use">HIT ME</button>
+    <modal :visible="visible" @receive="reuse">
+      <template v-slot:header>BLOOD</template>
+      <template v-slot:content>AIR</template>
+      <template v-slot:footer>GRASS</template>
+    </modal>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+       visible: false
+    }
+  },
+  components: {
+    Modal: Modal
+  },
+  methods: {
+    use () {
+      this.visible = true;
+    },
+    reuse () {
+      this.visible = false;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ lesson25/app/src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="test" :class="{'show': visible == true }">
+        <span @click="switcher" class="close">X</span>
+        <div><slot name="header"></slot></div>
+        <div><slot name="content"></slot></div>
+        <div><slot name="footer"></slot></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Modal',
+        props: {
+            visible: Boolean
+        },
+        methods: {
+            switcher () {
+                this.$emit("receive")
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .test {
+        width: 400px;
+        height: 500px;
+        display: flex;
+        visibility: hidden;
+        background-color: #a9a9a9;
+    }
+    .show {
+        visibility: visible;
+    }
+
+    .close {
+        cursor: pointer;
+        margin: 5px;
+        position: relative;
+        top: 10px;
+        left: 92%;
+    }
+</style>
