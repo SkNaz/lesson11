7573cf56112b4267bc22cfb238ebf28b5b79e543
SkNaz <58735171+SkNaz@users.noreply.github.com>
2020-02-27 17:00:47 +0200

    Add files via upload

--- lesson20/index.js
+++ lesson20/index.js
@@ -1,12 +1,17 @@
-var report = new XMLHttpRequest();
-var html = document.querySelector("body");
-var button = document.querySelector(".btn");
+window.onload = function() {
+	var report = new XMLHttpRequest();
+	var html = document.querySelector("body");
+	var button = document.querySelector(".btn");
 
+	report.onreadystatechange = function() {
+		if(report.readyState == 4) {
+			html.innerHTML += "<div>" + report.responseText + "</div>";
+		}
+	}
+	
+	report.open("get", "http://localhost:3000/user", true);
 
-report.open("get", "http://localhost:3000/user", true);
-
-report.send();
-
-button.onclick = function() {
-	html.innerHTML += "<div>" + report.responseText + "</div>";
+	button.onclick = function() {
+		report.send();
+	}
 }
