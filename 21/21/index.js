29f5e50fd1f8352b9c3de8ea974044d084da6128
SkNaz <58735171+SkNaz@users.noreply.github.com>
2020-03-09 11:14:21 +0200

    Add files via upload

--- 21/21/index.js
+++ 21/21/index.js
@@ -1,41 +1,39 @@
-var zapr = new XMLHttpRequest();
-var login = document.querySelector(".text_one");
-var pass = document.querySelector(".text_two");
-var xxx = new XMLHttpRequest();
-var html = document.querySelector("body");
+window.onload = function() {
+	var zapr = new XMLHttpRequest();
+	var login = document.querySelector(".text_one");
+	var pass = document.querySelector(".text_two");
+	var xxx = new XMLHttpRequest();
+	var wrap = document.querySelector("div");
 
-
-zapr.onreadystatechange = function() {
-	if(zapr.readyState == 4) {
-		let data = zapr.responseText;
-		let parsedData = JSON.parse(data);
-		console.log(parsedData);	
-	}
-}
-
-xxx.onreadystatechange = function() {
-	if(xxx.readyState == 4) {
-		let data = xxx.responseText;
-		/*let parsedData = JSON.parse(data);
-		console.log(parsedData);*/
-	}
-}
-
-zapr.open("post", "http://localhost:3000/user", true);
-xxx.open("post", "http://localhost:3000/goods", true);
-
-
-document.querySelector(".btn").onclick = function() {
-	const cred = {
-		login: login.value,
-		password:  pass.value
+	zapr.onreadystatechange = function() {
+		if(zapr.readyState == 4) {
+			if(zapr.status == 200) {
+				let data = zapr.responseText;
+				let parsedData = JSON.parse(data);
+				console.log(parsedData);
+				xxx.send(parsedData);
+			}
+		}	
 	}
 
-	zapr.send(JSON.stringify(cred));
+	xxx.onreadystatechange = function() {
+		if(xxx.readyState == 4) {
+			let nextData = xxx.responseText;
+			let nextParsedData = JSON.parse(nextData);
+			console.log(nextParsedData);
+			wrap.innerHTML = `${nextData}`
+		}
+	}
 
-	xxx.send(zapr.responseText);
-	
-	setTimeout(function ping() {
-		html.innerHTML += "<div>" + xxx.responseText + "</div>"
-	}, 1000);
+	zapr.open("post", "http://localhost:3000/user", true);
+	xxx.open("post", "http://localhost:3000/goods", true);
+
+	document.querySelector(".btn").onclick = function() {
+		const cred = {
+			login: login.value,
+			password:  pass.value
+		}
+
+		zapr.send(JSON.stringify(cred));
+	}
 }
