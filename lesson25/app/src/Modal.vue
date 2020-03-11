049b0e17b9426f8d5e085d826f375c4ad4bc55d4
SkNaz <58735171+SkNaz@users.noreply.github.com>
2020-03-11 12:35:03 +0200

    Add files via upload

---
+++ lesson25/app/src/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="test" :class="{'show': visible == true }">
+        <span @click="switcher" class="close">X</span>
+        <div><slot></slot></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Modal',
+        props: {
+            visible: Boolean
+        },
+        methods: {
+            switcher () {
+                this.$emit("receive")
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .test {
+        width: 400px;
+        height: 500px;
+        border-radius: 8px;
+        display: flex;
+        visibility: hidden;
+        background-color: #ffffff;
+    }
+    .show {
+        visibility: visible;
+    }
+    .close {
+        cursor: pointer;
+        margin: 5px;
+        position: relative;
+        top: 10px;
+        left: 92%;
+        color: red;
+    }
+</style>
